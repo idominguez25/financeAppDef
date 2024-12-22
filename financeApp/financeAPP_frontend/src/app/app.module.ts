b4737c76d8e56bc937982424bd581f98700de8a3
Ingrid Dominguez <78429412+idominguez25@users.noreply.github.com>
2024-12-22 16:18:38 +0100

    Unión backend y frontend

---
+++ financeApp/financeAPP_frontend/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule, NgIf } from '@angular/common';
+import { AppRoutingModule } from './app.routes';
+import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { GastosListComponent } from './gastos-list/gastos-list.component';
+import {MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
+
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    BrowserModule,FormsModule, NgIf,
+    AppRoutingModule,
+    CommonModule, RouterModule, MatButtonModule, 
+    MatFormFieldModule, MatInputModule,
+    MatButtonModule, MatDialogModule, ReactiveFormsModule
+  ],
+  providers: [provideHttpClient()],
+  bootstrap: []
+})
+export class AppModule { }
